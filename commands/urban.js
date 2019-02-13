20cd8ffb560629b833890a4ed76faf0b7b39cb67
TrueXRetro <dragonminner867@outlook.com>
2019-02-13 19:35:36 +0100

    Removed old code and updated the embed

--- commands/urban.js
+++ commands/urban.js
@@ -1,34 +1,26 @@
-const urban = require("relevant-urban"); 
+const urban = require("relevant-urban"); // `npm i relevant-urban`
 const Discord = require("discord.js"); 
 
-// We can call our command handler here
+// Command Handler
 exports.run = async (client, message, args, tools) => {
 
-    // First, we need to verify, that they wrote the text.
     if (!args[0]) return message.channel.send(`***Please specify some text!***`);
-    // This will return and send  message, (args[0] is the first word after the command)
 
-    // Now, we can fetch the word from Urbandictionary
     let res = await urban(args.join(' ')).catch(e => {
-        // Although, if a error is found (word not found), this catch() will run
         return message.channel.send("***Sorry, that word was not found!***");
     });
 
-    // Now, we can form the response embed
     const embed = new Discord.RichEmbed()
-        .setColor('RANDOM') // This sets a random color each time the response is sent.
-        .setTitle(res.word) // The title now holds the word you fetched
-        .setURL(res.urbanURL) // The title is now clickable, and leads to the urbandictionary page
+        .setColor('RANDOM') 
+        .setTitle(res.word) 
+        .setURL(res.urbanURL) 
         .setDescription(`**Definition:**\n*${res.definition}*\n\n**Example:**\n*${res.example}*`)
-        .addField('Author', res.author, true) // This adds an inline(by setting true) field, with the author
+        .addField('Author', res.author, true) 
         .addField('Rating', `**\`Upvotes: ${res.thumbsUp} | Downvotes: ${res.thumbsDown}\`**`)
 
-    // There are also some optional fileds(or fields that may be empty with a response)
     if (res.tags.length > 0 && res.tags.join(', ').length < 1024) {
-        // This verifies that there are tags, and they dont exced 1024 characters when joined together
-        embed.addField('Tags', res.tags.join(', '), true) // This also creates an inline field holding the tags
+        embed.addField('Tags', res.tags.join(', '), true) 
     }
 
-    // Now, we can send the response to chat
     message.channel.send(embed); 
 }
